ad552d8ef1f52039447c82fe5f54365fc91aa625
link.origogi(김정태)/kakao <link.origogi@kakaocorp.com>
2025-11-20 14:27:02 +0900

    route

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from crewai.flow.flow import Flow, listen, start, router, and_, or_
+from pydantic import BaseModel
 
-class MyFirstFlow(Flow):
+class MyState(BaseModel):
+    user_id : int = 1
+    is_admin : bool = False
+
+class MyFirstFlow(Flow[MyState]):
 
     @start()
     def first(self):
+        print(self.state.user_id)
         print("Hello, World!")
         return "first_done"
 
@@ -21,11 +27,33 @@ class MyFirstFlow(Flow):
     @listen(and_(second, third))
     def final(self):
         print("All steps completed!")
+        self.state.user_id = 100
         return "all_done"
+    
+    @router(final)
+    def route(self):
+        a = 2
+
+        if a % 2 == 0:
+            return "even"
+        else:
+            return "odd"
+        
+    
+    @listen('even')
+    def even_path(self):
+        print(self.state.user_id)
+
+        print("The number is even.")
+
+    @listen('odd')
+    def odd_path(self):
+        print("The number is odd.")
+
 
 flow = MyFirstFlow()
 
-flow.plot()
+# flow.plot()
 
 result = flow.kickoff()
 print(f"\nFlow result: {result}")
